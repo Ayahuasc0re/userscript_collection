f1306c59304c6ca927a4950c6a75ca2b513f98ca
Ayahuasc0re <->
2017-06-28 21:52:02 +0200

    javarchive.com Preview Direct Link - Update to 0.5.1

--- javarchive.com_previewDirectLink.js
+++ javarchive.com_previewDirectLink.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         javarchive.com - Preview Direct-Link
-// @version      0.5
+// @version      0.5.1
 // @description  Adds a direct link to the pixhost preview on javarchive.org
 // @author       Ayahuasc0re
 // @updateURL    https://raw.githubusercontent.com/ayahuasc0re/userscript_collection/master/javarchive.com_previewDirectLink.js
@@ -18,8 +18,11 @@
         // postContent.innerHTML = postContent.innerHTML.replace(/www\.pixhost\.org\/show/g, "img8.pixhost.org/images");
         // postContent.innerHTML = postContent.innerHTML.replace(/(www|t8)\.pixhost\.org\/(show|thumbs)/g, "img8.pixhost.org/images");
         // Correct hyperlink now?
-        var numRegex = /t(\d)/g;
+        var numRegex = /t(\d)\.pixhost\.org/g;
         var num = numRegex.exec(postContent.innerHTML)[1];
+        // console.log(postContent.innerHTML + " | " + num);
+        if (!num) return;
+        // var replaceStr = "img" + num + ".pixhost.org/images";
         postContent.innerHTML = postContent.innerHTML.replace(/((www|t\d)\.)?pixhost\.org\/(show|thumbs)/g, "img" + num + ".pixhost.org/images");
     }
     catch(err) {
